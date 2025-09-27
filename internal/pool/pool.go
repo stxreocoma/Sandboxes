f2cc9b9fa59a0604189fb5712ea0ab3bf9b9661b
stxreocoma <vvedenskii2005@gmail.com>
2025-09-27 15:54:41 +0300

    feat(pool): implemented initialization of pool

--- internal/pool/pool.go
+++ internal/pool/pool.go
@@ -1,6 +1,7 @@
 package pool
 
 import (
+	"log"
 	"sync"
 )
 
@@ -18,7 +19,39 @@ type pool struct {
 }
 
 func New(workerCount, taskQueueSize int, afterTask func()) Pool {
+	p := &pool{
+		tasks:     make(chan func(), taskQueueSize),
+		stopped:   make(chan struct{}),
+		afterTask: afterTask,
+	}
 
+	for workers := 0; workers < workerCount; workers++ {
+		go func() {
+			for task := range p.tasks {
+				func() {
+					defer func() {
+						if r := recover(); r != nil {
+							log.Println("Error while executing task in pool:", r)
+						}
+						p.wg.Done()
+					}()
+					task()
+				}()
+				if p.afterTask != nil {
+					func() {
+						defer func() {
+							if r := recover(); r != nil {
+								log.Println("Error while executing afterfunc in pool:", r)
+							}
+						}()
+						p.afterTask()
+					}()
+				}
+			}
+		}()
+	}
+
+	return p
 }
 
 func (p *pool) Submit(task func()) error {
